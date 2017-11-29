e267061e3890e2776357978d73525d07ec670447
skysnow <skysnow2017@gmail.com>
2017-11-29 17:31:23 +0800

    pymysql and spider

--- Practice/lore/python_spider.py
+++ Practice/lore/python_spider.py
@@ -19,16 +19,92 @@
 	将互联网上的URL对应的网页下载到本地的工具
 	urllib2下载网页的方法:
 		1.urlopen(url)
+		2.添加data,http header
+		3.添加特殊情景的处理器
+		    HTTPCookieProcessor:需要账户密码登陆的网站
+		    ProxyHandler:需要代理登陆的网站
+		    HTTPSHandler:需要https登陆的网站
+		    HTTPRedirectHandler:网站内部重定向
+		    统一传给urllib2.build_opener(handler)
+
 5.网页解析器(BeautifulSoup)
+    1.正则表达式re:模糊匹配
+    2.python自带的html.parser 结构化解析
+    3.beautifulSoup 结构化解析
+    4.lxml  结构化解析
 '''
 
-import urllib
-#直接请求
-response=urllib.urlopen('http://www.baidu.com')
+# import urllib2
+# #直接请求
+# response=urllib2.urlopen('http://www.baidu.com')
+#
+# #获取状态码，如果是200表示获取成功
+# print(response.getcode())
+#
+# #读取内容
+# cont=response.read()
+# print(cont)
 
-#获取状态码，如果是200表示获取成功
-print(response.getcode())
 
-#读取内容
-cont=response.read()
-print(cont)
+#创建request对象
+# request=urllib2.Requeset(url)
+#
+# #添加数据
+# request.add_data('a','1')
+#
+# #添加http的header
+# request.add_header('User-Agent','Mozilla/5.0')
+#
+# #发送请求获取结果
+# response=urllib2.urlopen(request)
+
+import urllib2,cookielib
+
+#创建cookie容器
+cj=cookielib.CookieJar()
+
+#创建一个opener
+opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+#给urllib2安装opener
+urllib2.install_opener(opener)
+
+#使用带有cookie的urllib2访问网页
+response=urllib2.urlopen('http://www.baidu.com')
+
+print response.getcode()
+
+
+#创建BeautifulSoup对象
+from bs4 import BeautifulSoup
+#根据网页字符串创建BeautifulSoup对象
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup=BeautifulSoup(html_doc,        #HTML文档字符串
+                   'html.parser',    #HTML解析器
+                   from_encoding='utf8'     #HTML文档的编码
+                   )
+
+#搜索节点(find_all,find)
+links=soup.find_all('a')
+#访问节点信息:以字典的形式访问
+for link in links:
+    print link.name,link['href'],link.get_text()
+
+#url格式，数据格式，页面编码
+
+'''
+扩展:需要登录，验证码，Ajax,服务器防爬虫，多线程，分布式
+'''
